d46903747b85810201c4237ebeac141897c053ab
vuhuy1101 <vuhuy1101@gmail.com>
2015-11-15 13:30:27 -0800

    Revert "add retrieveData"
    This reverts commit a304c99ca76754a8cb42c5475605f81691bea3ef.

--- retrieveData/src/retrieveData/retrieveData.java
+++ retrieveData/src/retrieveData/retrieveData.java
@@ -2,22 +2,25 @@ package retrieveData;
 import java.sql.*;
 import java.util.ArrayList;
 
-import config.MySQLConfig;
-
-public class retrieveData implements MySQLConfig{
+public class retrieveData {
 	public static void main(String[] args) {
-		Connection connection = null;
-		Statement statement = null;
-		ArrayList<Health> healthList = new ArrayList<Health>();
-		String SELECT_HEALTH_QUERY = "select * from health";
-		 
-		try {
-            String jdbcURL = "jdbc:mysql://" + MySQLConfig.host + ":" + MySQLConfig.port + "/" + MySQLConfig.database;
-            connection = DriverManager.getConnection(jdbcURL, MySQLConfig.user, MySQLConfig.password);
-            statement = connection.createStatement();
-
+		Connection conn = null;
+		String url = "jdbc:mysql://localhost:3306/cs157";
+		String driver = "com.mysql.jdbc.Driver";
+		String userName = "root";
+	    String password = "";
+	    Statement stmt = null;
+	    String SELECT_QUERY = "select * from health";  
+	    
+	    /*Lists store data retrieved from database*/
+	    ArrayList<Health> healthList = new ArrayList<Health>();
+	    try
+        {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, userName, password);
+            stmt = conn.createStatement();
             System.out.println("Connected to the database");
-            ResultSet rs = statement.executeQuery(SELECT_HEALTH_QUERY);
+            ResultSet rs = stmt.executeQuery(SELECT_QUERY);
             while (rs.next())
             {
             	Health healthData = new Health();
@@ -28,7 +31,7 @@ public class retrieveData implements MySQLConfig{
                 healthData.setYear(rs.getInt("year"));
                 healthList.add(healthData);
             }
-            connection.close();
+            conn.close();
             System.out.println("Disconnected from database");
         } catch (Exception e)
         {
