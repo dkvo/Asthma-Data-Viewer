74023cedca8b50fdb1c3b5591013523c84743e75
dkvo <dangkhoanvo@gmail.com>
2015-11-16 00:02:08 -0800

    set up GUI

---
+++ src/DataGUI/DataTable.java
@@ -0,0 +1,31 @@
+import java.sql.*;
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JTable;
+import javax.swing.table.*;
+import javax.swing.event.*;
+public class DataTable extends AbstractTableModel  {
+	public static void main(String[] args) {
+		
+	
+	}
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
